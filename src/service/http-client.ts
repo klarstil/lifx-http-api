d9d1ead20540ba69664e4f07634e34677db6c2ce
klarstil <klarstil@googlemail.com>
2019-12-13 16:50:46 +0100

    Removed request & response transformers

--- src/service/http-client.ts
+++ src/service/http-client.ts
@@ -1,26 +1,11 @@
 import axios, { AxiosInstance } from 'axios';
-import { toCamel, toSnake } from 'convert-keys';
 
 export default (bearerToken: string, apiVersion: string = 'v1'): AxiosInstance => {
     return axios.create({
         baseURL: `https://api.lifx.com/${apiVersion}/`,
-        transformResponse: [responseTransformer],
-        transformRequest: [requestTransformer],
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${bearerToken}`
         }
     });
 };
-
-export const responseTransformer = (data: string): object => {
-    const json: object = JSON.parse(data);
-
-    return toCamel(json);
-};
-
-export const requestTransformer = (data: object): string => {
-    const transformedData: object = toSnake(data);
-
-    return JSON.stringify(transformedData);
-}
