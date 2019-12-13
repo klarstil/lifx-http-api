d7124d06cf28183c71cdf1cf39c1156858e675bd
klarstil <klarstil@googlemail.com>
2019-12-13 14:42:16 +0100

    Adds coverage report and restructures test specs

--- __tests__/criteria/advanced-color-crtieria.spec.ts
+++ __tests__/criteria/advanced-color-crtieria.spec.ts
@@ -1,109 +1,111 @@
 import AdvancedColorCriteria from '../../src/criteria/advanced-color-crtieria';
 
-it('should allow chaining', () => {
-    const criteria = new AdvancedColorCriteria();
-    expect(criteria.setHue(0) instanceof AdvancedColorCriteria).toBe(true);
-});
-
-it('should accept valid hue values', () => {
-    const criteria = new AdvancedColorCriteria();
+describe('advanced color criteria', () => {
+    it('should allow chaining', () => {
+        const criteria = new AdvancedColorCriteria();
+        expect(criteria.setHue(0) instanceof AdvancedColorCriteria).toBe(true);
+    });
     
-    criteria.setHue(0);
-    expect(criteria.hue).toBe(0);
-    expect(criteria.getColor()).toBe('hue:0');
-
-    criteria.setHue(20);
-    expect(criteria.hue).toBe(20);
-    expect(criteria.getColor()).toBe('hue:20');
-
-    expect(() => {
-        criteria.setHue(-1);
-    }).toThrow();
-
-    expect(() => {
-        criteria.setHue(361);
-    }).toThrow();
-});
-
-it('should accept valid saturation values', () => {
-    const criteria = new AdvancedColorCriteria();
+    it('should accept valid hue values', () => {
+        const criteria = new AdvancedColorCriteria();
+        
+        criteria.setHue(0);
+        expect(criteria.hue).toBe(0);
+        expect(criteria.getColor()).toBe('hue:0');
     
-    criteria.setSaturation(0);
-    expect(criteria.saturation).toBe(0);
-    expect(criteria.getColor()).toBe('saturation:0');
-
-    criteria.setSaturation(20);
-    expect(criteria.saturation).toBe(0.2);
-    expect(criteria.getColor()).toBe('saturation:0.2');
-
-    expect(() => {
-        criteria.setSaturation(-1);
-    }).toThrow();
-
-    expect(() => {
-        criteria.setSaturation(101);
-    }).toThrow();
-});
-
-it('should accept valid brightness values', () => {
-    const criteria = new AdvancedColorCriteria();
+        criteria.setHue(20);
+        expect(criteria.hue).toBe(20);
+        expect(criteria.getColor()).toBe('hue:20');
     
-    criteria.setBrightness(0);
-    expect(criteria.brightness).toBe(0);
-    expect(criteria.getColor()).toBe('brightness:0');
-
-    criteria.setBrightness(20);
-    expect(criteria.brightness).toBe(0.2);
-    expect(criteria.getColor()).toBe('brightness:0.2');
-
-    expect(() => {
-        criteria.setBrightness(-1);
-    }).toThrow();
-
-    expect(() => {
-        criteria.setBrightness(101);
-    }).toThrow();
-});
-
-it('should accept valid kelvin values', () => {
-    const criteria = new AdvancedColorCriteria();
+        expect(() => {
+            criteria.setHue(-1);
+        }).toThrow();
     
-    criteria.setKelvin(1500);
-    expect(criteria.kelvin).toBe(1500);
-    expect(criteria.getColor()).toBe('kelvin:1500');
-
-    criteria.setKelvin(9000);
-    expect(criteria.kelvin).toBe(9000);
-    expect(criteria.getColor()).toBe('kelvin:9000');
-
-    expect(() => {
-        criteria.setKelvin(0);
-    }).toThrow();
-
-    expect(() => {
-        criteria.setKelvin(10000);
-    }).toThrow();
-});
-
-it('should combine hue, saturation and kelvin values', () => {
-    const criteria = new AdvancedColorCriteria();
-
-    criteria.setHue(20);
-    expect(criteria.hue).toBe(20);
-
-    criteria.setBrightness(20);
-    expect(criteria.brightness).toBe(0.2);
-
-    criteria.setSaturation(20);
-    expect(criteria.saturation).toBe(0.2);
+        expect(() => {
+            criteria.setHue(361);
+        }).toThrow();
+    });
     
-    criteria.setKelvin(1500);
-    expect(criteria.kelvin).toBe(1500);
+    it('should accept valid saturation values', () => {
+        const criteria = new AdvancedColorCriteria();
+        
+        criteria.setSaturation(0);
+        expect(criteria.saturation).toBe(0);
+        expect(criteria.getColor()).toBe('saturation:0');
     
-    expect(criteria.getColor()).toBe('kelvin:1500 brightness:0.2 saturation:0.2 hue:20');
-});
-
-it('should return null if no color property is provided', () => {
-    const criteria = new AdvancedColorCriteria();
-    expect(criteria.getColor()).toBe(null);
+        criteria.setSaturation(20);
+        expect(criteria.saturation).toBe(0.2);
+        expect(criteria.getColor()).toBe('saturation:0.2');
+    
+        expect(() => {
+            criteria.setSaturation(-1);
+        }).toThrow();
+    
+        expect(() => {
+            criteria.setSaturation(101);
+        }).toThrow();
+    });
+    
+    it('should accept valid brightness values', () => {
+        const criteria = new AdvancedColorCriteria();
+        
+        criteria.setBrightness(0);
+        expect(criteria.brightness).toBe(0);
+        expect(criteria.getColor()).toBe('brightness:0');
+    
+        criteria.setBrightness(20);
+        expect(criteria.brightness).toBe(0.2);
+        expect(criteria.getColor()).toBe('brightness:0.2');
+    
+        expect(() => {
+            criteria.setBrightness(-1);
+        }).toThrow();
+    
+        expect(() => {
+            criteria.setBrightness(101);
+        }).toThrow();
+    });
+    
+    it('should accept valid kelvin values', () => {
+        const criteria = new AdvancedColorCriteria();
+        
+        criteria.setKelvin(1500);
+        expect(criteria.kelvin).toBe(1500);
+        expect(criteria.getColor()).toBe('kelvin:1500');
+    
+        criteria.setKelvin(9000);
+        expect(criteria.kelvin).toBe(9000);
+        expect(criteria.getColor()).toBe('kelvin:9000');
+    
+        expect(() => {
+            criteria.setKelvin(0);
+        }).toThrow();
+    
+        expect(() => {
+            criteria.setKelvin(10000);
+        }).toThrow();
+    });
+    
+    it('should combine hue, saturation and kelvin values', () => {
+        const criteria = new AdvancedColorCriteria();
+    
+        criteria.setHue(20);
+        expect(criteria.hue).toBe(20);
+    
+        criteria.setBrightness(20);
+        expect(criteria.brightness).toBe(0.2);
+    
+        criteria.setSaturation(20);
+        expect(criteria.saturation).toBe(0.2);
+        
+        criteria.setKelvin(1500);
+        expect(criteria.kelvin).toBe(1500);
+        
+        expect(criteria.getColor()).toBe('kelvin:1500 brightness:0.2 saturation:0.2 hue:20');
+    });
+    
+    it('should return null if no color property is provided', () => {
+        const criteria = new AdvancedColorCriteria();
+        expect(criteria.getColor()).toBe(null);
+    })
 })
--- __tests__/criteria/selector-criteria.spec.ts
+++ __tests__/criteria/selector-criteria.spec.ts
@@ -1,153 +1,155 @@
 import SelectorCriteria from '../../src/criteria/selector-criteria';
 
-it('should allow chaining', () => {
-    const criteria = new SelectorCriteria();
-    expect(criteria.setAll() instanceof SelectorCriteria).toBe(true);
-});
-
-it('should set an "all" selector', () => {
-    let criteria = new SelectorCriteria();
-    expect(
-        criteria.setAll().getSelector()
-    ).toBe('all');
-
-    criteria = new SelectorCriteria();
-    expect(
-        criteria.setAll(true).getSelector()
-    ).toBe('all:random');
-});
-
-it('should set an "id" selector', () => {
-    let criteria = new SelectorCriteria();
-    expect(
-        criteria.setId('adf1334').getSelector()
-    ).toBe('id:adf1334');
-
-    criteria = new SelectorCriteria();
-    expect(() => {
-        criteria.setId('').getSelector()
-    }).toThrow();
-});
-
-it('should set a "label" selector', () => {
-    let criteria = new SelectorCriteria();
-    expect(
-        criteria.setLabel('KitchenLamp').getSelector()
-    ).toBe('label:KitchenLamp');
-
-    criteria = new SelectorCriteria();
-    expect(() => {
-        criteria.setLabel('').getSelector()
-    }).toThrow();
-});
-
-it('should set a "group" selector', () => {
-    let criteria = new SelectorCriteria();
-    expect(
-        criteria.setGroup('Kitchen').getSelector()
-    ).toBe('group:Kitchen');
-
-    // random flag
-    criteria = new SelectorCriteria();
-    expect(
-        criteria.setGroup('Kitchen', true).getSelector()
-    ).toBe('group:Kitchen:random');
-
-    criteria = new SelectorCriteria();
-    expect(() => {
-        criteria.setGroup('').getSelector()
-    }).toThrow();
-});
-
-it('should set a "group id" selector', () => {
-    let criteria = new SelectorCriteria();
-    expect(
-        criteria.setGroupId('adf1334').getSelector()
-    ).toBe('group_id:adf1334');
-
-    // random flag
-    criteria = new SelectorCriteria();
-    expect(
-        criteria.setGroupId('adf1334', true).getSelector()
-    ).toBe('group_id:adf1334:random');
-
-    criteria = new SelectorCriteria();
-    expect(() => {
-        criteria.setGroupId('').getSelector()
-    }).toThrow();
-});
-
-it('should set a "location" selector', () => {
-    let criteria = new SelectorCriteria();
-    expect(
-        criteria.setLocation('Home').getSelector()
-    ).toBe('location:Home');
-
-    // random flag
-    criteria = new SelectorCriteria();
-    expect(
-        criteria.setLocation('Home', true).getSelector()
-    ).toBe('location:Home:random');
-
-    criteria = new SelectorCriteria();
-    expect(() => {
-        criteria.setLocation('').getSelector()
-    }).toThrow();
-});
-
-it('should set a "location id" selector', () => {
-    let criteria = new SelectorCriteria();
-    expect(
-        criteria.setLocationId('adf1334').getSelector()
-    ).toBe('location_id:adf1334');
-
-    // random flag
-    criteria = new SelectorCriteria();
-    expect(
-        criteria.setLocationId('adf1334', true).getSelector()
-    ).toBe('location_id:adf1334:random');
-
-    criteria = new SelectorCriteria();
-    expect(() => {
-        criteria.setLocationId('').getSelector()
-    }).toThrow();
-});
-
-it('should set a "scene id" selector', () => {
-    let criteria = new SelectorCriteria();
-    expect(
-        criteria.setSceneId('adf1334').getSelector()
-    ).toBe('scene_id:adf1334');
-
-    // random flag
-    criteria = new SelectorCriteria();
-    expect(
-        criteria.setSceneId('adf1334', true).getSelector()
-    ).toBe('scene_id:adf1334:random');
-
-    criteria = new SelectorCriteria();
-    expect(() => {
-        criteria.setSceneId('').getSelector()
-    }).toThrow();
-});
-
-it('should throw an error when the selector chain exceeds 25', () => {
-    const generateRandomId = (): string => {
-        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    }
-
-    const criteria = new SelectorCriteria();
-
-    for(let i = 0; i <= 26; i++) {
-        criteria.setId(generateRandomId());
-    }
-
-    expect(() => {
-        criteria.getSelector();
-    }).toThrow();
-});
-
-it('should return null if no selector is provided', () => {
-    const criteria = new SelectorCriteria();
-    expect(criteria.getSelector()).toBe(null);
+describe('selector criteria', () => {
+    it('should allow chaining', () => {
+        const criteria = new SelectorCriteria();
+        expect(criteria.setAll() instanceof SelectorCriteria).toBe(true);
+    });
+    
+    it('should set an "all" selector', () => {
+        let criteria = new SelectorCriteria();
+        expect(
+            criteria.setAll().getSelector()
+        ).toBe('all');
+    
+        criteria = new SelectorCriteria();
+        expect(
+            criteria.setAll(true).getSelector()
+        ).toBe('all:random');
+    });
+    
+    it('should set an "id" selector', () => {
+        let criteria = new SelectorCriteria();
+        expect(
+            criteria.setId('adf1334').getSelector()
+        ).toBe('id:adf1334');
+    
+        criteria = new SelectorCriteria();
+        expect(() => {
+            criteria.setId('').getSelector()
+        }).toThrow();
+    });
+    
+    it('should set a "label" selector', () => {
+        let criteria = new SelectorCriteria();
+        expect(
+            criteria.setLabel('KitchenLamp').getSelector()
+        ).toBe('label:KitchenLamp');
+    
+        criteria = new SelectorCriteria();
+        expect(() => {
+            criteria.setLabel('').getSelector()
+        }).toThrow();
+    });
+    
+    it('should set a "group" selector', () => {
+        let criteria = new SelectorCriteria();
+        expect(
+            criteria.setGroup('Kitchen').getSelector()
+        ).toBe('group:Kitchen');
+    
+        // random flag
+        criteria = new SelectorCriteria();
+        expect(
+            criteria.setGroup('Kitchen', true).getSelector()
+        ).toBe('group:Kitchen:random');
+    
+        criteria = new SelectorCriteria();
+        expect(() => {
+            criteria.setGroup('').getSelector()
+        }).toThrow();
+    });
+    
+    it('should set a "group id" selector', () => {
+        let criteria = new SelectorCriteria();
+        expect(
+            criteria.setGroupId('adf1334').getSelector()
+        ).toBe('group_id:adf1334');
+    
+        // random flag
+        criteria = new SelectorCriteria();
+        expect(
+            criteria.setGroupId('adf1334', true).getSelector()
+        ).toBe('group_id:adf1334:random');
+    
+        criteria = new SelectorCriteria();
+        expect(() => {
+            criteria.setGroupId('').getSelector()
+        }).toThrow();
+    });
+    
+    it('should set a "location" selector', () => {
+        let criteria = new SelectorCriteria();
+        expect(
+            criteria.setLocation('Home').getSelector()
+        ).toBe('location:Home');
+    
+        // random flag
+        criteria = new SelectorCriteria();
+        expect(
+            criteria.setLocation('Home', true).getSelector()
+        ).toBe('location:Home:random');
+    
+        criteria = new SelectorCriteria();
+        expect(() => {
+            criteria.setLocation('').getSelector()
+        }).toThrow();
+    });
+    
+    it('should set a "location id" selector', () => {
+        let criteria = new SelectorCriteria();
+        expect(
+            criteria.setLocationId('adf1334').getSelector()
+        ).toBe('location_id:adf1334');
+    
+        // random flag
+        criteria = new SelectorCriteria();
+        expect(
+            criteria.setLocationId('adf1334', true).getSelector()
+        ).toBe('location_id:adf1334:random');
+    
+        criteria = new SelectorCriteria();
+        expect(() => {
+            criteria.setLocationId('').getSelector()
+        }).toThrow();
+    });
+    
+    it('should set a "scene id" selector', () => {
+        let criteria = new SelectorCriteria();
+        expect(
+            criteria.setSceneId('adf1334').getSelector()
+        ).toBe('scene_id:adf1334');
+    
+        // random flag
+        criteria = new SelectorCriteria();
+        expect(
+            criteria.setSceneId('adf1334', true).getSelector()
+        ).toBe('scene_id:adf1334:random');
+    
+        criteria = new SelectorCriteria();
+        expect(() => {
+            criteria.setSceneId('').getSelector()
+        }).toThrow();
+    });
+    
+    it('should throw an error when the selector chain exceeds 25', () => {
+        const generateRandomId = (): string => {
+            return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        }
+    
+        const criteria = new SelectorCriteria();
+    
+        for(let i = 0; i <= 26; i++) {
+            criteria.setId(generateRandomId());
+        }
+    
+        expect(() => {
+            criteria.getSelector();
+        }).toThrow();
+    });
+    
+    it('should return null if no selector is provided', () => {
+        const criteria = new SelectorCriteria();
+        expect(criteria.getSelector()).toBe(null);
+    });
 });
